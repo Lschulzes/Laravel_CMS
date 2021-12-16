647f226700d620960f34bdd228d06c2a7ecca8da
lschulzes <lucassilvaiee@gmail.com>
2021-12-16 16:26:03 -0300

    Refactor blogpostcontroller

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -4,13 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Events\CommentPosted;
 use App\Http\Requests\StoreComment;
-use App\Jobs\NotifyUsersPostWasCommented;
-use App\Jobs\ThrottleMail;
-use App\Mail\CommentPostedMarkdown;
-use App\Mail\CommentPostedOnPostWatched;
 use App\Models\BlogPost;
-use App\Models\User;
-use Illuminate\Support\Facades\Mail;
 
 class PostCommentController extends Controller
 {
